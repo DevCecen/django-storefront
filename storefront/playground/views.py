f5014c19cd478cede9acde2b3b8d06bbd2c7f0dc
cecen <191208060+DevCecen@users.noreply.github.com>
2026-04-09 17:46:48 +0300

    query set metotları üzerinde çalışmalar yapıldı

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from store.models import Product
 from store.models import OrderItem
+from store.models import Order
 
 # Create your views here.
 
@@ -10,8 +11,6 @@ my_information = {
 }
 
 def say_hello(request):
-    query_set = Product.objects.filter(
-        id__in=OrderItem.objects.values('product_id').distinct()
-    ).order_by('title')
+    query_set = Order.objects.select_related('customer').prefetch_related('orderitem_set__product').order_by('-placed_at')[:5]
         
     return render(request, 'hello.html' , {'name' : 'Ali', 'products':list(query_set)})
