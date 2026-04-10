3b646f9d8563bb86bde50393ae3eb7aa2dda2db5
cecen <191208060+DevCecen@users.noreply.github.com>
2026-04-10 20:08:14 +0300

    Django databases function denemeleri yapıldı

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -3,6 +3,8 @@ from django.http import HttpResponse
 from store.models import Product
 from store.models import OrderItem
 from store.models import Order
+from store.models import Customer
+from django.db.models import Func,Value,F
 
 # Create your views here.
 
@@ -11,6 +13,8 @@ my_information = {
 }
 
 def say_hello(request):
-    query_set = Order.objects.select_related('customer').prefetch_related('orderitem_set__product').order_by('-placed_at')[:5]
+    query_set = Customer.objects.annotate(full_name=Func(F('first_name'),Value(' ') ,F('last_name'), function='CONCAT'))
         
     return render(request, 'hello.html' , {'name' : 'Ali', 'products':list(query_set)})
+
+
