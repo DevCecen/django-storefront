d30e34b5d08741dc9235163232b021d781d5c86d
cecen <191208060+DevCecen@users.noreply.github.com>
2026-04-17 21:11:00 +0300

    Query Setlerin üzerinde sorgu çalışmaları yapıldı

--- storefront/playground/views.py
+++ storefront/playground/views.py
@@ -1,20 +1,21 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.contrib.contenttypes.models import ContentType
 from store.models import Product
-from store.models import OrderItem
-from store.models import Order
-from store.models import Customer
-from django.db.models import Func,Value,F
+from tags.models import TaggedItem
+
 
 # Create your views here.
 
-my_information = {
-    'İsim' : 'Ali'
-}
 
 def say_hello(request):
-    query_set = Customer.objects.annotate(full_name=Func(F('first_name'),Value(' ') ,F('last_name'), function='CONCAT'))
-        
-    return render(request, 'hello.html' , {'name' : 'Ali', 'products':list(query_set)})
+    
+    content_type = ContentType.objects.get_for_model(Product)
+    
+    query_set = TaggedItem.objects.select_related('tag').filter(
+        content_type = content_type,
+        object_id = 1
+    )
+    
+    return render(request,'hello.html',{'name':'Ali','result':list(query_set)}) 
 
 
